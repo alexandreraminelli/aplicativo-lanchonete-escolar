d733e15c13c9e7eaa1466e033ba53f2e4ee9f524
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-11-20 20:19:56 -0300

    feat(constants): adicionar constantes de tamanhos para dimensões, espaçamentos e cantos arredondados

---
+++ gosnack_client/lib/utils/constants/styles/sizes.dart
@@ -0,0 +1,24 @@
+/// Valores constantes de tamanhos para dimensões, espaçamentos,
+/// cantos arredondados, tamanhos de fontes, etc.
+class Sizes {
+  // -- Espaçamentos e dimensões -------------------------------------------- //
+  static const double xs = 8.0;
+  static const double sm = 12.0;
+  static const double md = 16.0;
+  static const double lg = 22.0;
+  static const double xl = 36.0;
+  static const double xl2 = 48.0;
+  static const double xl3 = 80.0;
+  static const double xl4 = 120.0;
+  static const double xl5 = 224.0;
+  static const double xl6 = 288.0;
+  static const double xl7 = 384.0;
+  static const double xl8 = 512.0;
+  static const double xl9 = 640.0;
+
+  // -- Border Radius ------------------------------------------------------- //
+  static const double radiusSm = 8.0;
+  static const double radiusMd = 12.0;
+  static const double radiusLg = 14.0;
+  static const double radiusFull = 100.0;
+}
