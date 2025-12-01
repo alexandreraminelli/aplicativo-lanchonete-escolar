64d575bc30a734c204a405ecff4d64bed6260282
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-01 16:54:27 -0300

    feat(on-boarding-screen): adicionar arquivos Lottie das animações

---
+++ gosnack_client/lib/utils/constants/content/animations_strings.dart
@@ -0,0 +1,11 @@
+/// Strings com os caminhos para as animações Lottie na pasta `assets/animations/`
+class KAnimations {
+  /// Pasta base de assets de animações
+  static const _animationsPath = "assets/animations";
+
+  // -- Animações
+  static const String customerMenuOptions =
+      "$_animationsPath/customer_menu_options.json";
+  static const String pickingUpOrder = "$_animationsPath/picking_up_order.json";
+  static const String servingDome = "$_animationsPath/serving_dome.json";
+}
