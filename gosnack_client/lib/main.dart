ba4f9c3e140692cf952fa592d351a316d49f1986
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-01 17:18:23 -0300

    feat(local-storage): iniciar o armazenamento local do GetX na inicialização do app

--- gosnack_client/lib/main.dart
+++ gosnack_client/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:gosnack_client/app.dart';
 
 /// Ponto de entrada da aplicação.
-void main() {
+void main() async {
   // Widgets Binding
   // Garante que o Flutter esteja inicializado corretamente antes de executar o app
   final WidgetsBinding widgetsBindings =
       WidgetsFlutterBinding.ensureInitialized();
 
-  // TODO: Iniciar armazenamento local do GetX
+  // Iniciar armazenamento local do GetX
   // Garante que o armazenamento local esteja pronto antes de prosseguir
+  await GetStorage.init();
 
   // Preservar Splash Screen enquanto os demais itens são inicializados
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBindings);
