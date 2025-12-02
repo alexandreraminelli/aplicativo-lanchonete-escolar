e1468242f48fd960b76051e725d912ec2b932c8b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-02 12:41:32 -0300

    feat(images): criar arquivo de constantes de caminhos para imagens nos assets e adicionar caminhos das logos

---
+++ gosnack_client/lib/utils/constants/content/image_strings.dart
@@ -0,0 +1,15 @@
+/// Strings com os caminhos para arquivos de imagens na pasta `assets/`.
+class KImages {
+  // -- Logos --------------------------------------------------------------- //
+  static const String favicon = "assets/logos/favicon.png";
+  static const String logoWhite = "assets/logos/logo_white.png";
+  static const String logoBlack = "assets/logos/logo_black.png";
+  static const String brandingWhite = "assets/logos/branding_white.png";
+  static const String brandingBlack = "assets/logos/branding_black.png";
+
+  static const String faviconSvg = "assets/logos/favicon.svg";
+  static const String logoWhiteSvg = "assets/logos/logo_white.svg";
+  static const String logoBlackSvg = "assets/logos/logo_black.svg";
+  static const String brandingWhiteSvg = "assets/logos/branding_white.svg";
+  static const String brandingBlackSvg = "assets/logos/branding_black.svg";
+}
