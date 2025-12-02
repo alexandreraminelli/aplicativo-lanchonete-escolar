a22d4eb125692fc08385e0ebe0c1ad3f87fd243a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-02 16:45:29 -0300

    feat(text-strings): constantes de strings de textos para a OnBoarding Page

---
+++ gosnack_client/lib/utils/constants/content/text_strings.dart
@@ -0,0 +1,17 @@
+/// Constantes de strings de textos utilizados no aplicativo.
+class KTexts {
+  const KTexts._(); // evitar instanciação
+
+  // -- Global Texts -------------------------------------------------------- //
+
+  // -- OnBoarding Screen Texts --------------------------------------------- //
+  static const String onBoardingTitle1 = "Bem-vindo ao GoSnack";
+  static const String onBoardingSubTitle1 =
+      "O jeito mais rápido e fácil de pedir seus lanches dentro do colégio. Evite filas e aproveite seus intervalos ao máximo!";
+  static const String onBoardingTitle2 = "Explore o cardápio da sua unidade";
+  static const String onBoardingSubTitle2 =
+      "Selecione a lanchonete do seu colégio, descubra o cardápio disponível e monte seu pedido do seu jeito!";
+  static const String onBoardingTitle3 = "Pague e Retire Sem Complicações";
+  static const String onBoardingSubTitle3 =
+      "Finalize o pedido pelo app e retire quando estiver pronto. Rápido, seguro e sem filas!";
+}
