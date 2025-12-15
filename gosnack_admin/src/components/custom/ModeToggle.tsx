97b4defc4818824a0297c461cc7cd2dcea44fc88
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-15 18:58:35 -0300

    refactor(shadcn): mover e configurar arquivos do Shadcn para dentro da pasta `src/`

--- gosnack_admin/src/components/custom/ModeToggle.tsx
+++ gosnack_admin/src/components/custom/ModeToggle.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Button } from "@/src/components/ui/button"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/src/components/ui/dropdown-menu"
 import { LucideIcon, MonitorIcon, MoonIcon, SunIcon } from "lucide-react"
 import { useTheme } from "next-themes"
 
