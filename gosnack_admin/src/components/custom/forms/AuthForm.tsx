44528b46bbde1d1f7a54c3094adf0f29b2562c8f
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-16 16:06:52 -0300

    feat(auth-form): cabeçalho do formulário

--- gosnack_admin/src/components/custom/forms/AuthForm.tsx
+++ gosnack_admin/src/components/custom/forms/AuthForm.tsx
@@ -1,3 +1,4 @@
+import { FieldGroup } from "@/src/components/ui/field"
 import { cn } from "@/src/lib/utils"
 import { type ClassValue } from "clsx"
 
@@ -6,7 +7,26 @@ interface Props {
   className?: ClassValue
 }
 
-/** Formulário de login. */
+/** Formulário de autenticação. */
 export default function AuthForm({ className, ...props }: Props) {
-  return <form className={cn("", className)} {...props}></form>
+  return (
+    <form className={cn("", className)} {...props}>
+      <FieldGroup>
+        <AuthFormHeader />
+      </FieldGroup>
+    </form>
+  )
+}
+
+/** Cabeçalho do formulário de autenticação. */
+function AuthFormHeader() {
+  const title = "GoSnack: Painel de Controle"
+  const subtitle = "Acesse o sistema para gerenciar pedidos e produtos."
+
+  return (
+    <header className="flex flex-col items-center gap-2 text-center">
+      <h1 className="text-2xl font-bold">{title}</h1>
+      <p className="text-muted-foreground text-balance">{subtitle}</p>
+    </header>
+  )
 }
