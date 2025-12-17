eb79e30684ed386f01545c712ae56389511f547b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-17 14:00:07 -0300

    feat(logos): adicionar svgs das logos

--- gosnack_admin/src/constants/images.ts
+++ gosnack_admin/src/constants/images.ts
@@ -1,6 +1,17 @@
 /** Constantes de path de rotas da aplicação. */
 export const IMAGES = {
-  logo: "",
+  /* Logos */
+  logos: {
+    favicon: "/logos/favicon.svg",
+    full: {
+      white: "/logos/logo-white.svg",
+      black: "/logos/logo-black.svg",
+    },
+    branding: {
+      white: "/logos/branding-white.svg",
+      black: "/logos/branding-black.svg",
+    },
+  },
 
   /* Login Page */
   loginBg: "/images/photos/login-bg.jpg",
