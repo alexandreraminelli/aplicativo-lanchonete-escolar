ae593deda43e3b3ef8323a74ca974442e2d35b84
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-12-29 18:13:43 -0300

    feat(auth): redirecionar usuários autenticados para o dashboard e usuários não autenticados para a página de login

--- gosnack_admin/src/app/(auth)/layout.tsx
+++ gosnack_admin/src/app/(auth)/layout.tsx
@@ -1,6 +1,19 @@
+import { ROUTES } from "@/src/constants/routes"
+import { firebaseApp } from "@/src/lib/firebase/clientApp"
+import { getAuth } from "firebase/auth"
+import { redirect } from "next/navigation"
+
 /**
  * Layout de autenticação.
  */
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  const auth = getAuth(firebaseApp) // instância do serviço Auth
+
+  if (auth.currentUser) {
+    // redirecionar funcionários autenticados para o dashboard
+    redirect(ROUTES.dashboard)
+  }
+
+  // Componente principal
   return <>{children}</>
 }
--- gosnack_admin/src/app/(private)/layout.tsx
+++ gosnack_admin/src/app/(private)/layout.tsx
@@ -1,9 +1,20 @@
+import { ROUTES } from "@/src/constants/routes"
+import { firebaseApp } from "@/src/lib/firebase/clientApp"
+import { getAuth } from "firebase/auth"
+import { redirect } from "next/navigation"
+
 /**
  * Layout das páginas privadas.
  * Acessíveis apenas após a autenticação.
  */
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
-  // TODO: redirecionar funcionários não autenticados para a página de login
+  const auth = getAuth(firebaseApp) // instância do serviço Auth
 
+  if (!auth.currentUser) {
+    // redirecionar funcionários não autenticados para a página de login
+    redirect(ROUTES.login)
+  }
+
+  // Componente principal
   return <>{children}</>
 }
