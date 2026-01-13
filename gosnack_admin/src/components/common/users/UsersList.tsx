4c108ca8ea0e1126a61cadb4c39547445118b7ae
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-13 17:58:27 -0300

    feat(users): obter lista de usuários no componente `UsersList`

--- gosnack_admin/src/components/common/users/UsersList.tsx
+++ gosnack_admin/src/components/common/users/UsersList.tsx
@@ -1,4 +1,39 @@
+"use client"
+
+import { USERS_TEXTS } from "@/constants/texts/users.texts"
+import { useUsers } from "@/hooks/queries/users/user.queries"
+import { toast } from "sonner"
+
 /** Lista de usuários no sistema. */
 export default function UsersList() {
-  return <section>TODO: Lista de Usuários</section>
+  // Buscar usuários usando React Query
+  const { data: users = [], isLoading, isError } = useUsers()
+
+  // TODO: Skeleton durante carregamento
+  if (isLoading) return <p>Carregando Usuários...</p>
+
+  // Mensagem de erro
+  if (isError) {
+    toast.error(USERS_TEXTS.errors.getAll.title, { description: USERS_TEXTS.errors.getAll.message })
+  }
+
+  return (
+    <>
+      {/* Quantidade de usuários */}
+      <p className="text-muted-foreground mb-4">{USERS_TEXTS.quant(users.length)}</p>
+
+      {/* Tabela de usuários */}
+      <section>
+        {users.map((user) => (
+          <div className="bg-muted border-2 m-5 p-4 rounded-2xl" key={user.id}>
+            <p>
+              {user.firstName} {user.lastName}
+            </p>
+            <p>{user.email}</p>
+            <p>Papel: {user.role}</p>
+          </div>
+        ))}
+      </section>
+    </>
+  )
 }
--- gosnack_admin/src/constants/texts/users.texts.ts
+++ gosnack_admin/src/constants/texts/users.texts.ts
@@ -6,5 +6,14 @@ export const USERS_TEXTS = {
     disable: "Desativar Usuário",
   },
 
+  errors: {
+    getAll: {
+      title: "Erro ao Buscar Usuários",
+      message: "Ocorreu um erro ao carregar a lista de usuários. Por favor, tente novamente mais tarde.",
+    },
+  },
+
   management: "Gerenciar Usuários",
+
+  quant: (count: number) => `${count} usuário${count !== 1 ? "s" : ""} no sistema`,
 } as const
