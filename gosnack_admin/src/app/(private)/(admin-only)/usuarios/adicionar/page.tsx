f03086071e9d23c989a42c4cdde3e4e3300c5ec4
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-14 19:53:21 -0300

    feat(users): definir layout e estilo da página de adicionar usuário

--- gosnack_admin/src/app/(private)/(admin-only)/usuarios/adicionar/page.tsx
+++ gosnack_admin/src/app/(private)/(admin-only)/usuarios/adicionar/page.tsx
@@ -1,12 +1,22 @@
 import AuthForm from "@/components/forms/AuthForm"
+import { IMAGES } from "@/constants/images"
+import { NAV_TEXTS } from "@/constants/texts/navigation.texts"
+import Image from "next/image"
 
 /** Página de adicionar novo usuário. */
 export default function AddUserPage() {
   return (
-    <div>
-      Adicionar Usuário
+    <div className="flex flex-row gap-10 justify-around">
       {/* Form de cadastro */}
-      <AuthForm mode="sign-up" />
+      <main className="space-y-6 w-full max-w-xl">
+        <h2 className="font-semibold text-3xl md:text-4xl text-center md:text-start">{NAV_TEXTS.addUser}</h2>
+        <AuthForm mode="sign-up" />
+      </main>
+
+      {/* Ilustração */}
+      <aside className=" max-md:hidden max-w-2/5 self-start mt-10 sticky top-20 transition-all">
+        <Image src={IMAGES.illustrations.addUser} alt="" width={752} height={556} className="object-cover" />
+      </aside>
     </div>
   )
 }
--- gosnack_admin/src/constants/images.ts
+++ gosnack_admin/src/constants/images.ts
@@ -1,24 +1,28 @@
+const ILLUSTRATIONS_PATH = "/images/illustrations"
+const LOGOS_PATH = "/logos"
+
 /** Constantes de path de rotas da aplicação. */
 export const IMAGES = {
   // Logos
   logos: {
-    favicon: "/logos/favicon.svg",
+    favicon: `${LOGOS_PATH}/favicon.svg`,
     full: {
-      white: "/logos/logo-white.svg",
-      black: "/logos/logo-black.svg",
+      white: `${LOGOS_PATH}/logo-white.svg`,
+      black: `${LOGOS_PATH}/logo-black.svg`,
     },
     branding: {
-      white: "/logos/branding-white.svg",
-      black: "/logos/branding-black.svg",
+      white: `${LOGOS_PATH}/branding-white.svg`,
+      black: `${LOGOS_PATH}/branding-black.svg`,
     },
   },
 
   // Ilustrações
   illustrations: {
-    accessDenied: "/images/illustrations/access-denied.svg",
-    empty: "/images/illustrations/empty.svg",
-    notFound: "/images/illustrations/not-found.svg",
-    schoolUnit: "/images/illustrations/school-unit.svg",
+    accessDenied: `${ILLUSTRATIONS_PATH}/access-denied.svg`,
+    addUser: `${ILLUSTRATIONS_PATH}/add-user.svg`,
+    empty: `${ILLUSTRATIONS_PATH}/empty.svg`,
+    notFound: `${ILLUSTRATIONS_PATH}/not-found.svg`,
+    schoolUnit: `${ILLUSTRATIONS_PATH}/school-unit.svg`,
   },
 
   // Página de login
