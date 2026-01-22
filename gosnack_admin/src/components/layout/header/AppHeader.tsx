5eaed8607269174e559fd79530943bfdb221cf9b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-22 17:58:04 -0300

    feat(header): adicionar efeito de blur no header

--- gosnack_admin/src/components/layout/header/AppHeader.tsx
+++ gosnack_admin/src/components/layout/header/AppHeader.tsx
@@ -6,7 +6,7 @@ import HeaderBreadcrumb from "./HeaderBreadcrumb"
 /** Cabeçalho do aplicativo. */
 export default function AppHeader() {
   return (
-    <header className="bg-background sticky top-0 z-50 flex w-full items-center border-b">
+    <header className="bg-background/75 backdrop-blur sticky top-0 z-50 flex w-full items-center border-b">
       <div className="flex w-full items-center gap-2 px-4 h-16">
         {/* Botão de exibir/ocultar sidebar */}
         <SidebarTrigger size="icon" />
