6a660d7c8f8fcac05a88cdaabefa3c3821196c12
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-26 17:54:46 -0300

    feat(unit-cafeteria): criar use cases da funcionalidade de selecionar unidade e lanchonete

---
+++ gosnack_client/lib/features/unit_cafeteria_selection/domain/use_cases/get_cafeterias_by_unit.dart
@@ -0,0 +1,22 @@
+import 'package:gosnack_client/features/unit_cafeteria_selection/domain/entities/cafeteria.dart';
+import 'package:gosnack_client/features/unit_cafeteria_selection/domain/interfaces/cafeteria_repository.dart';
+
+/// Use Case para obter todas as lanchonetes de uma unidade escolar específica
+/// no repositório.
+class GetCafeteriasByUnit {
+  // -- Public Constructor -------------------------------------------------- //
+  GetCafeteriasByUnit(this.repository);
+
+  // -- Instance Variables -------------------------------------------------- //
+
+  /// Repositório de lanchonetes.
+  final CafeteriaRepository repository;
+
+  // -- Public Methods ------------------------------------------------------ //
+
+  /// Executa o Use Case para obter todas as lanchonetes de uma unidade
+  /// escolar específica.
+  Future<List<Cafeteria>> call(String unitId) {
+    return repository.getCafeteriasByUnitId(unitId);
+  }
+}
---
+++ gosnack_client/lib/features/unit_cafeteria_selection/domain/use_cases/get_units.dart
@@ -0,0 +1,20 @@
+import 'package:gosnack_client/features/unit_cafeteria_selection/domain/entities/unit.dart';
+import 'package:gosnack_client/features/unit_cafeteria_selection/domain/interfaces/unit_repository.dart';
+
+/// Use Case para obter todas as unidades escolares do repositório.
+class GetUnits {
+  // -- Public Constructor -------------------------------------------------- //
+  GetUnits(this.repository);
+
+  // -- Instance Variables -------------------------------------------------- //
+
+  /// Repositório de unidades escolares.
+  final UnitRepository repository;
+
+  // -- Public Methods ------------------------------------------------------ //
+
+  /// Executa o Use Case para obter todas as unidades escolares.
+  Future<List<Unit>> call() {
+    return repository.getAllUnits();
+  }
+}
