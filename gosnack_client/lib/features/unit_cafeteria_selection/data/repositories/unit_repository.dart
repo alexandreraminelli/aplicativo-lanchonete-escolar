4a127af8a769cc784e8f6e19b471c330fbd4e2a0
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-26 15:38:51 -0300

    feat(cafeterias): criar e organizar pasta de feature `unit_cafeteria_selector/`

---
+++ gosnack_client/lib/features/unit_cafeteria_selection/data/repositories/unit_repository.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+/// Repositório para operações na coleção de unidades escolares no Firestore.
+class UnitRepository extends GetxController{
+  
+}
