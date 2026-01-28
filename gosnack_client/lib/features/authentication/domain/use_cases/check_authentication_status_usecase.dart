1baf49d3f4cfe001153c47d7158a04a5a2e36357
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-28 16:19:18 -0300

    feat(auth): criar caso de uso para verificar o status da autenticação do usuário

---
+++ gosnack_client/lib/features/authentication/domain/use_cases/check_authentication_status_usecase.dart
@@ -0,0 +1,20 @@
+import 'package:gosnack_client/features/authentication/domain/interfaces/authentication_repository.dart';
+
+/// UseCase para verificar o status de autenticação do usuário.
+class CheckAuthenticationStatusUsecase {
+  // -- Private Instance Variables ------------------------------------------ //
+
+  /// Repositório de autenticação.
+  final AuthenticationRepository _repository;
+
+  // -- Public Constructor -------------------------------------------------- //
+
+  CheckAuthenticationStatusUsecase(this._repository);
+
+  // -- Public Methods ------------------------------------------------------ //
+
+  /// Verifica se o usuário está autenticado.
+  bool call() {
+    return _repository.isAuthenticated();
+  }
+}
