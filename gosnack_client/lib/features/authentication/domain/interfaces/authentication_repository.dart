221cb994b57790d1d1acf2be17cec49abeeb3132
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-01-28 15:32:21 -0300

    feat(auth): definir contrato do repositório de autenticação

--- gosnack_client/lib/features/authentication/domain/interfaces/authentication_repository.dart
+++ gosnack_client/lib/features/authentication/domain/interfaces/authentication_repository.dart
@@ -1,4 +1,33 @@
-/// Interface com as funcionalidades e interações relacionadas a autenticação.
+import 'package:gosnack_client/features/authentication/domain/entities/user_entity.dart';
+
+/// Interface com o contrato do repositório de autenticação.
+///
+/// Define os métodos que devem ser implementados para a autenticação
+/// do usuário.
 abstract class AuthenticationRepository {
-  
+  // -- Authentication ------------------------------------------------------ //
+
+  /// Obtém o usuário autenticado atualmente.
+  /// Retorna `null` se não houver.
+  UserEntity? getCurrentUser();
+
+  /// Efetua login com e-mail e senha.
+  Future<UserEntity> loginWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  /// Efetua logout do usuário atual.
+  Future<void> logout();
+
+  /// Verifica se o usuário está autenticado.
+  Future<void> isAuthenticated();
+
+  // -- OnBoarding ---------------------------------------------------------- //
+
+  /// Verifica se o usuário já passou pela tela de onboarding (boas-vindas).
+  bool isFirstTime();
+
+  /// Marca que o usuário já passou pela tela de onboarding (boas-vindas).
+  Future<void> setFirstTimeFalse();
 }
